dbe278cc59ced87acbd7ee81722fec24641cde14
toxar42 <64099002+toxar42@users.noreply.github.com>
2023-03-10 09:56:31 +0300

    Update secpage.js

--- proj1/JS/secpage.js
+++ proj1/JS/secpage.js
@@ -15,36 +15,36 @@ function Generate(n)
 {
     for (let i = 0;i < n;i++)
     maincont.insertAdjacentHTML('beforeend',
-    `<div class="catalogpage">
+    `
         <div class="page">
         <img alt="sdfvsd" class="img_page" src="${arrayRandElement(catalog_img)}">
         <div class="blockheartgen"><img class="smallph" src="../RESOURSES/heart.png" onclick="rotate(this)"></div>
-        <p class="name">${arrayRandElement(catalog_text_description)} ${arrayRandElement(catalog_text)}</p><p class="name">
+        <p class="name">${arrayRandElement(catalog_text_description)} ${arrayRandElement(catalog_text)}</p><p class="page_txt">
         <img alt="sdfvsd" class="smallph" src="../RESOURSES/Tag.png">${arrayRandElement(catalog_city)}<img alt="sdfvsd" src="../RESOURSES/star.png">${Math.round(getRandomNumber(3,5)*100)/100}</p>
         </div>
         <div class="page">
         <img alt="sdfvsd" class="img_page" src="${arrayRandElement(catalog_img)}">
         <div class="blockheartgen"><img class="smallph" src="../RESOURSES/heart.png" onclick="rotate(this)"></div>
-        <p class="name">${arrayRandElement(catalog_text_description)} ${arrayRandElement(catalog_text)}</p><p class="name">
+        <p class="name">${arrayRandElement(catalog_text_description)} ${arrayRandElement(catalog_text)}</p><p class="page_txt">
         <img alt="sdfvsd" class="smallph" src="../RESOURSES/Tag.png">${arrayRandElement(catalog_city)}<img alt="sdfvsd" src="../RESOURSES/star.png">${Math.round(getRandomNumber(3,5)*100)/100}</p>
         </div>
         <div class="page">
         <img alt="sdfvsd" class="img_page" src="${arrayRandElement(catalog_img)}">
         <div class="blockheartgen"><img class="smallph" src="../RESOURSES/heart.png" onclick="rotate(this)"></div>
-        <p class="name">${arrayRandElement(catalog_text_description)} ${arrayRandElement(catalog_text)}</p><p class="name">
+        <p class="name">${arrayRandElement(catalog_text_description)} ${arrayRandElement(catalog_text)}</p><p class="page_txt">
         <img alt="sdfvsd" class="smallph" src="../RESOURSES/Tag.png">${arrayRandElement(catalog_city)}<img alt="sdfvsd" src="../RESOURSES/star.png">${Math.round(getRandomNumber(3,5)*100)/100}</p>
         </div>
         <div class="page">
         <img alt="sdfvsd" class="img_page" src="${arrayRandElement(catalog_img)}">
         <div class="blockheartgen"><img class="smallph" src="../RESOURSES/heart.png" onclick="rotate(this)"></div>
-        <p class="name">${arrayRandElement(catalog_text_description)} ${arrayRandElement(catalog_text)}</p><p class="name">
+        <p class="name">${arrayRandElement(catalog_text_description)} ${arrayRandElement(catalog_text)}</p><p class="page_txt">
         <img alt="sdfvsd" class="smallph" src="../RESOURSES/Tag.png">${arrayRandElement(catalog_city)}<img alt="sdfvsd" src="../RESOURSES/star.png">${Math.round(getRandomNumber(3,5)*100)/100}</p>
         </div>
-    </div>`); 
+    `); 
     }
 Generate(10);
 let h=1;
-let ch = document.querySelector('.catalogpage').clientHeight;
+let ch = document.querySelector('.page').clientHeight;
 maincont.onscroll = function()
 {   
     if(maincont.scrollTop > ch * h ){
@@ -52,5 +52,32 @@ maincont.onscroll = function()
     h++;
     }
 }
-console.log(document.querySelector('.catalogpage').scrollTop);
+let sear_val = document.getElementById('search');
+sear_val.oninput = function()
+{
+    let cont_values = document.querySelectorAll('.name');
+    let search_val = document.getElementById('search');
 
+    if (search_val.value != ''){
+    cont_values.forEach( cont_val=> {
+        if (cont_val.innerText.search(search_val.value) != -1)
+        {
+            console.log(true);
+            cont_val.parentElement.classList.remove('page_none');
+            cont_val.parentElement.classList.add('page');
+        }
+        else
+        {
+            cont_val.parentElement.classList.add('page_none');
+        }
+        });
+    }
+    else
+    {
+        cont_values.forEach( cont_val=> {
+  
+                cont_val.parentElement.classList.remove('page_none');
+                cont_val.parentElement.classList.add('page');
+    })
+    }
+}
